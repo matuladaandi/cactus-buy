36b10537e47e9cf46cec476b972d64cd6cd4a8cc
matuladaandi@gmail.com <matuladaandi@gmail.com>
2024-03-06 13:32:34 +0800

    news section done and fitur blue navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,9 +25,17 @@ const linkAction = () => {
   // when we click on each nav__link, we remove the show-menu class
   navMenu.classList.remove("show-menu");
 };
-navLink.forEach(n => n.addEventListener("click", linkAction));
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 // ========== ADD BLUR HEADER ==========
+const blurHeader = () => {
+  const header = document.getElementById("header");
+  // Add class if the bottom offsite is greater than 50 of the value
+  this.scrollY >= 50
+    ? header.classList.add("blur-header")
+    : header.classList.remove("blur-header");
+};
+window.addEventListener("scroll", blurHeader);
 
 // ========== SHOW SCROLL UP ==========
 
