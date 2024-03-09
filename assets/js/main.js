93929604ee2654299952316197663ee7c82e4419
matuladaandi@gmail.com <matuladaandi@gmail.com>
2024-03-09 17:29:42 +0800

    scroll section done

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,7 +38,34 @@ const blurHeader = () => {
 window.addEventListener("scroll", blurHeader);
 
 // ========== SHOW SCROLL UP ==========
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  // when the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("scroll-up");
+};
+window.addEventListener("scroll", scrollUp);
 
 // ========== SCROLL SECTIONS ACTIVE LINK ==========
+const sections = document.querySelectorAll("section[id]");
 
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionClass.classList.add("active-link");
+    } else {
+      sectionClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
 // ========== SCROLL REVEAL ANIMATION ==========
